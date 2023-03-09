bde444781644be9df61a57843a488f15d26230c7
tony <toonyrenner@gmail.com>
2023-03-09 20:38:06 -0300

    Creating project

---
+++ src/main/java/com/github/itstoony/sales/JavaSalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.itstoony.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSalesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSalesApiApplication.class, args);
+    }
+
+}
